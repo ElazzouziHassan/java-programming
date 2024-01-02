23b65882b3d86fcd23df7175fadd7df7415478ff
wizardy <wizardhassan99@gmail.com>
2024-01-02 12:14:16 +0100

    commit

---
+++ Salaires/src/Employe.java
@@ -0,0 +1,20 @@
+public abstract class Employe {
+
+  private String nom;
+  private String prenom;
+  private int age;
+  private String dateEntree;
+
+  public Employe(String nom, String prenom, int age, String dateEntree) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+    this.dateEntree = dateEntree;
+  }
+
+  public abstract double calculerSalaire();
+
+  public String getNom() {
+    return "L'employé " + prenom + " " + nom;
+  }
+}
