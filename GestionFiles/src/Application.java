ef35ecba5e99bc5e6bb5ca22b8797e52cc474e49
wizardy <wizardhassan99@gmail.com>
2024-01-02 10:02:51 +0100

    commit

---
+++ GestionFiles/src/Application.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class Application {
+
+  public static void main(String[] args) {
+    File file = new File("null");
+    if(file.exists()){
+      for (String str: file.list()){
+        // File newFile = new File(file.getAbsolute File());
+      }
+    }
+  }
+}
