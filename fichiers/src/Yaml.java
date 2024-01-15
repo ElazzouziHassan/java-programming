5cf4e9a8ebf62ca9daa191cdcb6a1c7552094d9e
wizardy <wizardhassan99@gmail.com>
2024-01-15 20:42:01 +0100

    commit

---
+++ fichiers/src/Yaml.java
@@ -0,0 +1,11 @@
+import java.io.FileReader;
+import java.util.Map;
+
+public class Yaml {
+
+  public Map<String, Map<String, String>> load(FileReader reader) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'load'");
+  }
+
+}
